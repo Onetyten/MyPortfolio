09d6ff9d5361ced2198e96dba61d34cfb4f58f65
Adetayo <onetyten@gmail.com>
2024-08-23 04:10:33 +0100

    images rename

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Intro_nav from '../Intro/Intro_nav'
 import Intro_text from '../Intro/Intro_text'
 import Skill_card from './Skill_card'
+import ReactImg from './react.png'
 
 export default function Skills() {
   return (
@@ -9,7 +10,7 @@ export default function Skills() {
       <h3 className=' text-white text-3xl lg:text-5xl font-bold block text-center pt-10'>My Skills</h3>
       <div className='flex justify-center flex-wrap w-full pl-10 py-10 lg:p-16' >
 
-        <Skill_card name="React" src ="Images/Skill icons/icons8-react-a-javascript-library-for-building-user-interfaces-96.png" skillPoint = "h-3/6" percent="60%"/>
+        <Skill_card name="React" src ={ReactImg} skillPoint = "h-3/6" percent="60%"/>
         <Skill_card name="HTML" src ="Images/Skill icons/icons8-html-480.svg" skillPoint = "h-full" percent="100"/>
         <Skill_card name="CSS" src ="Images/Skill icons/icons8-css-480.svg" skillPoint = "h-3/6" percent="80%"/>
         <Skill_card name="JavaScript" src ="Images/Skill icons/icons8-javascript.svg" skillPoint = "h-4/6" percent="80%"/>
