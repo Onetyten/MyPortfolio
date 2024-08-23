6a917ac008f9ac9cbb17518cb8d135c4bfe4ad07
Adetayo <onetyten@gmail.com>
2024-08-23 04:25:11 +0100

    Skill icon path update

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Skill_card from './Skill_card'
 import ReactImg from './react.png'
+import HtmlImg from './html.svg'
+import CssImg from './css.svg'
+import JavascriptImg from './javascript.svg'
+import TailwindImg from './tailwind.svg'
+import PythonImg from './python.svg'
+import FigmaImg from './figma.svg'
+import GithubImg from './github.png'
+
 
 
 export default function Skills() {
@@ -10,13 +18,13 @@ export default function Skills() {
       <div className='flex justify-center flex-wrap w-full pl-10 py-10 lg:p-16' >
 
         <Skill_card name="React" src ={ReactImg} skillPoint = "h-3/6" percent="60%"/>
-        <Skill_card name="HTML" src ="Images/Skill icons/icons8-html-480.svg" skillPoint = "h-full" percent="100"/>
-        <Skill_card name="CSS" src ="Images/Skill icons/icons8-css-480.svg" skillPoint = "h-3/6" percent="80%"/>
-        <Skill_card name="JavaScript" src ="Images/Skill icons/icons8-javascript.svg" skillPoint = "h-4/6" percent="80%"/>
-        <Skill_card name="Tailwind" src ="Images/Skill icons/icons8-tailwindcss.svg" skillPoint = "h-4/6" percent="80%"/>
-        <Skill_card name="Python" src ="Images/Skill icons/icons8-python-480.svg" skillPoint = "h-3/6" percent="60%"/>
-        <Skill_card name="Figma" src ="Images/Skill icons/icons8-figma-480.svg" skillPoint = "h-4/6" percent="80%" />
-        <Skill_card name="Github" src ="Images/Skill icons/icons8-github-480.png" skillPoint = "h-3/6" percent="60%"/>
+        <Skill_card name="HTML" src ={HtmlImg} skillPoint = "h-full" percent="100"/>
+        <Skill_card name="CSS" src ={CssImg} skillPoint = "h-3/6" percent="80%"/>
+        <Skill_card name="JavaScript" src ={JavascriptImg} skillPoint = "h-4/6" percent="80%"/>
+        <Skill_card name="Tailwind" src ={TailwindImg} skillPoint = "h-4/6" percent="80%"/>
+        <Skill_card name="Python" src ={PythonImg} skillPoint = "h-3/6" percent="60%"/>
+        <Skill_card name="Figma" src ={FigmaImg} skillPoint = "h-4/6" percent="80%" />
+        <Skill_card name="Github" src ={GithubImg} skillPoint = "h-3/6" percent="60%"/>
         
 
         
