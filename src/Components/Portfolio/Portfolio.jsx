f4f91a4600fa2bcf011a9fc2a2afc9578e2b5993
Adetayo <onetyten@gmail.com>
2024-08-23 04:35:32 +0100

    image link revive

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import PortfolioCard from './PortfolioCard'
+import MaurysKitchen from './Maury.png'
+import Cara from './Cara1.png'
+import Apod from './Apod.png'
+import Blackjack from './Blackjack.png'
+import Aexus from './AexusAir.png'
 
 export default function Portfolio() {
 
@@ -10,15 +15,15 @@ export default function Portfolio() {
         <h3 className=' text-white text-3xl lg:text-5xl font-bold block text-center pt-10'>Portfolio</h3>
         <div className='lg:flex grid justify-center flex-wrap w-full px-16 pt-16 pb-8'>
           
-                <PortfolioCard src="Images/WebImages/Maury.png" name="Maurys Kitchen"desc ="This dynamic restaurant website streamlines order communication, allowing customers to place table orders instantly transmitted to the kitchen. The centralized dashboard enables efficient delivery." link ="https://mauryskitchen.netlify.app/"  />
+                <PortfolioCard src={MaurysKitchen} name="Maurys Kitchen"desc ="This dynamic restaurant website streamlines order communication, allowing customers to place table orders instantly transmitted to the kitchen. The centralized dashboard enables efficient delivery." link ="https://mauryskitchen.netlify.app/"  />
 
-                <PortfolioCard src="Images/WebImages/Cara1.png" name="Cara Medical Institute" desc="This medical website is designed to connect you to a medical professional as urgently as possible" link ="https://carahealth.netlify.app/" />
+                <PortfolioCard src={Cara}  name="Cara Medical Institute" desc="This medical website is designed to connect you to a medical professional as urgently as possible" link ="https://carahealth.netlify.app/" />
 
-                <PortfolioCard src="Images/WebImages/Apod.png" name="NASA APOD" desc="This NASA Picture of the Day app delivers stunning daily images from space, accompanied by detailed descriptions. Explore the cosmos, discover breathtaking photos, and learn about the universe right from your device with a new image every day." link ="https://cosmicexplorer.netlify.app/" />
+                <PortfolioCard src={Apod}  name="NASA APOD" desc="This NASA Picture of the Day app delivers stunning daily images from space, accompanied by detailed descriptions. Explore the cosmos, discover breathtaking photos, and learn about the universe right from your device with a new image every day." link ="https://cosmicexplorer.netlify.app/" />
 
-                <PortfolioCard src="Images/WebImages/Blackjack.png" name="Caravan Blackjack" desc ="This Blackjack app offers a classic casino experience where you can play the popular card game against the dealer. Test your skills, strategize, and try to hit 21 without going over in this engaging and easy-to-play app." link ="https://mauryskitchen.netlify.app/"/>
+                <PortfolioCard src={Blackjack}  name="Caravan Blackjack" desc ="This Blackjack app offers a classic casino experience where you can play the popular card game against the dealer. Test your skills, strategize, and try to hit 21 without going over in this engaging and easy-to-play app." link ="https://mauryskitchen.netlify.app/"/>
 
-                <PortfolioCard src="Images/WebImages/AexusAir.png" name="Aexus Air" desc="This tour agency provides curated travel experiences, offering guided tours to breathtaking destinations in Las Vegas. From adventure-packed itineraries to cultural explorations, we create unforgettable journeys tailored to your preferences." link ="https://mauryskitchen.netlify.app/" />
+                <PortfolioCard src={Aexus}  name="Aexus Air" desc="This tour agency provides curated travel experiences, offering guided tours to breathtaking destinations in Las Vegas. From adventure-packed itineraries to cultural explorations, we create unforgettable journeys tailored to your preferences." link ="https://mauryskitchen.netlify.app/" />
                 
                 
 
