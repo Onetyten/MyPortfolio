97bec3a4f00f811f1c51f6da5923534f08ef33d0
labae <onetyten@gmail.com>
2024-09-20 15:22:24 +0100

    solidus

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import MaurysKitchen from './Maury.png'
 import Cara from './Cara1.png'
 import Apod from './Apod.png'
 import Fitflow from'./Fitflow.png'
-import Blackjack from './Blackjack.png'
+import Solidus from './Solidus.png'
 
 
 export default function Portfolio() {
@@ -22,10 +22,10 @@ export default function Portfolio() {
 
                 <PortfolioCard src={Apod}  name="NASA APOD" desc="This NASA Picture of the Day app delivers stunning daily images from space, accompanied by detailed descriptions. Explore the cosmos, discover breathtaking photos, and learn about the universe right from your device with a new image every day." link ="https://cosmicexplorer.netlify.app/" />
 
-                {/* <PortfolioCard src={Blackjack}  name="Caravan Blackjack" desc ="This Blackjack app offers a classic casino experience where you can play the popular card game against the dealer. Test your skills, strategize, and try to hit 21 without going over in this engaging and easy-to-play app." link ="https://mauryskitchen.netlify.app/"/>*/}
+                <PortfolioCard src={Solidus}  name="Solidus Team Manager" desc ="As the Team Manager for our project assignment system, you will oversee the organization and allocation of teams to various projects. You can manage project tags, monitor team member counts, and ensure smooth transitions between teams as projects evolve. Optimize team efficiency, and ensure successful project delivery." link ="https://solidusteammanager.netlify.app/"/>
 
                 <PortfolioCard src={Fitflow}  name="Fitflow" desc=" The ultimate fitness companion designed to help you achieve your health goals through personalized workout plans to put you on the path towards a stronger, healthier you." link ="https://fitflowlive.netlify.app/" /> 
-                
+              
                 
 
            
